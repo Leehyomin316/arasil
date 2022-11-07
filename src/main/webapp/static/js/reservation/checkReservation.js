9edc861420a51de6617be9b14691caf52d626b9a
unknown <8536022@gmail.com>
2022-11-07 11:23:05 +0900

    alert재수정

--- src/main/webapp/static/js/reservation/checkReservation.js
+++ src/main/webapp/static/js/reservation/checkReservation.js
@@ -61,7 +61,7 @@
 		if(!confirm('예약을 취소하시겠습니까?')){
 			return false;
 		}
-		alert("예약취소되었습니다1");
+		alert("예약취소되었습니다");
 		var url = "cancelReserv";
 		url = url + "?reservSeq="+reservSeq.value;
 		location.href = url;
